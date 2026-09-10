550f442fad80ffe81ced46c01a1a39d367930db1
pinquinn2010 <quinndeklein@gmail.com>
2026-09-10 12:07:55 +0200

    week 1
    vlag
    huis
    stoplicht

--- Week 1/sketch.js
+++ Week 1/sketch.js
@@ -1,7 +1,60 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1900, 1000);
 }
 
 function draw() {
   background(220);
+fill('blue');
+rect(0, 200, 500, 100);
+fill('white');
+rect(0, 100, 500, 100);
+fill('red');
+rect(0, 0, 500, 100);
+textSize(50);
+fill("black");
+text('Quinn.de.klein', 50, 400);
+fill("black");
+square(50,600,195);
+fill("black");
+square(50,600,66,)
+fill("white");
+square(50,665,66,)
+fill("black");
+square(50,730,66,)
+fill("white");
+square(115,730,66,)
+fill("black");
+square(115,665,66,)
+fill("white");
+square(115,600,66,)
+fill("black");
+square(180,600,66,)
+fill("white");
+square(180,665,66,)
+fill("black");
+square(180,730,66,)
+// huis
+fill(230,730,66,40)
+square(500,800,200,)
+triangle(500,800,700,800,600,700)
+circle(600,750,50)
+rect(650,900,50,100)
+circle(660,950,15)
+square(520,875,50)
+
+// stoplicht
+
+fill("gray");
+rect(600,0,100,300)
+fill("red");
+circle(650,50,50)
+fill("yellow");
+circle(650,150,50)
+fill("green");
+circle(650,250,50)
+fill("gray");
+rect(635,300,35,100)
+// dobbelsteen
+fill("white");
+square(800,600,100)
 }
