4cb37af1e706a0e777e06610a32c1e5f469d2a22
pinquinn2010 <quinndeklein@gmail.com>
2026-09-24 12:39:18 +0200

    Update sketch.js

--- Week 3/sketch.js
+++ Week 3/sketch.js
@@ -1,22 +1,25 @@
 //tic tac toe
 
-let nummer1 = 0
-let nummer2 = 0
-let nummer4 = 0
-let nummer5 = 0
-let nummer6 = 0
-let nummer7 = 0
-let nummer8 = 0
-let nummer9 = 0
+let beurt = 0
+let nummer1 = 'white'
+let nummer2 = 'white'
+let nummer3 = 'white'
+let nummer4 = 'white'
+let nummer5 = 'white'
+let nummer6 = 'white'
+let nummer7 = 'white'
+let nummer8 = 'white'
+let nummer9 = 'white'
 
-
-
+function changeturn ()
+{
+beurt +=1
+}
 function setup() {
   createCanvas(600, 600);
 }
 
 function draw() {
-
 
   background(220);
   fill('red')
@@ -25,15 +28,23 @@ function draw() {
   rect(300, 0, 600, 600)
   fill('GRAY')
   square(50, 50, 475)
-  fill('white')
+  fill (nummer1)
   rect(75, 75, 125, 125, 10)
+  fill (nummer2)
   rect(225, 75, 125, 125, 10)
+  fill(nummer3)
   rect(375, 75, 125, 125, 10)
+  fill(nummer4)
   rect(75, 225, 125, 125, 10)
+  fill(nummer5)
   rect(225, 225, 125, 125, 10)
+  fill(nummer6)
   rect(375, 225, 125, 125, 10)
+  fill(nummer7)
   rect(75, 375, 125, 125, 10)
+  fill(nummer8)
   rect(225, 375, 125, 125, 10)
+  fill(nummer9)
   rect(375, 375, 125, 125, 10)
 
   if (mouseX > 75 && mouseX < 200) {
@@ -52,48 +63,127 @@ function draw() {
   if (nummer2 == 1) {
     fill('red')
     textSize(155)
-    text('X', 236, 193)
+    text('X', 236, 193) }
+ 
+ 
 
-  }
- if (nummer1 ==1){
-  fill('blue');
- } else {
-  fill ('white');
- }
-  rect(75, 75, 125, 125, 10);
+ 
+
 }
 
-function mousePressed() {
+function mouseClicked() {
 
-if (mouseX > 75 && mouseX < 200 &&
-    mouseY > 75 && mouseY < 200) {
-  nummer1 = 1;
+  //red
+  if (beurt == 0 || beurt == 2 || beurt == 4 ||beurt == 6 || beurt == 8 || beurt == 10 ){
+if (nummer1=='white'){
+ if (mouseX >= 75 && mouseX <= 200 && mouseY >= 75 && mouseY <= 200) {
+  nummer1 = 'red'
+  changeturn()
 }
-
-
-
-
-  if (nummer1 == 0) {
-    if (mouseX > 75 && mouseX < 200 &&
-      mouseY > 75 && mouseY
-    ) {
-      nummer1 = 1
+}
+  if (nummer2 == 'white') {
+    if (mouseX > 225 && mouseX < 350 && mouseY > 75 && mouseY < 200) {
+      nummer2 = 'red'
+      changeturn()
     }
   }
-  if (mouseX > 75 && mouseX < 200 &&
-    mouseY > 75 && mouseY
-  ) {
-    nummer1 = 1
-  }
-
-  if (nummer2 == 0) {
-    if (mouseX > 225 && mouseX < 200 &&
-      mouseY > 225 && mouseY < 300
-    ) {
-      nummer2 = 1
+  if (nummer3 == 'white') {
+    if (mouseX > 375 && mouseX < 500 && mouseY > 75 && mouseY < 200) {
+      nummer3 = 'red'
+      changeturn()
     }
-   
   }
-
-
+   if (nummer4 == 'white') {
+    if (mouseX >= 75 && mouseX <= 200 && mouseY >= 225 && mouseY <= 350) {
+      nummer4 = 'red'
+      changeturn()
+    }
+  }
+  if (nummer5 == 'white') {
+    if (mouseX > 225 && mouseX < 350 && mouseY > 225 && mouseY < 350) {
+      nummer5 = 'red'
+      changeturn()
+    }
+  }
+  if (nummer6 == 'white') {
+    if (mouseX > 375 && mouseX < 500 && mouseY > 225 && mouseY < 350) {
+      nummer6 = 'red'
+      changeturn()
+    }
+  }
+  if (nummer7 == 'white') {
+    if (mouseX >= 75 && mouseX <= 200 && mouseY >= 375 && mouseY <= 500) {
+      nummer7 = 'red'
+      changeturn()
+    }
+  }
+  if (nummer8 == 'white') {
+    if (mouseX > 225 && mouseX < 350 && mouseY > 375 && mouseY < 500) {
+      nummer8 = 'red'
+      changeturn()
+    }
+  }
+  if (nummer9 == 'white') {
+    if (mouseX > 375 && mouseX < 500 && mouseY > 375 && mouseY < 500) {
+      nummer9 = 'red'
+      changeturn()
+    }
+  }
+}
+  if (beurt == 1 || beurt == 3 || beurt == 5 || beurt == 7 || beurt == 9 || beurt == 11){
+    if (nummer1 == 'white') {
+      if (mouseX >= 75 && mouseX <= 200 && mouseY >= 75 && mouseY <= 200) {
+        nummer1 = 'blue'
+        changeturn()
+      }
+    }
+  if (nummer2 == 'white') {
+    if (mouseX > 225 && mouseX < 350 && mouseY > 75 && mouseY < 200) {
+      nummer2 = 'blue'
+      changeturn()
+    }
+  }
+  if (nummer3 == 'white') {
+    if (mouseX > 375 && mouseX < 500 && mouseY > 75 && mouseY < 200) {
+      nummer3 = 'blue'
+      changeturn()
+    }
+  }
+  if (nummer4 == 'white') {
+    if (mouseX >= 75 && mouseX <= 200 && mouseY >= 225 && mouseY <= 350) {
+      nummer4 = 'blue'
+      changeturn()
+    }
+  }
+  if (nummer5 == 'white') {
+    if (mouseX > 225 && mouseX < 350 && mouseY > 225 && mouseY < 350) {
+      nummer5 = 'blue'
+      changeturn()
+    }
+  }
+  if (nummer6 == 'white') {
+    if (mouseX > 375 && mouseX < 500 && mouseY > 225 && mouseY < 350) {
+      nummer6 = 'blue'
+      changeturn()
+    }
+  }
+  if (nummer7 == 'white') {
+    if (mouseX >= 75 && mouseX <= 200 && mouseY >= 375 && mouseY <= 500) {
+      nummer7 = 'blue'
+      changeturn()
+    }
+  }
+  if (nummer8 == 'white') {
+    if (mouseX > 225 && mouseX < 350 && mouseY > 375 && mouseY < 500) {
+      nummer8 = 'blue'
+      changeturn()
+    }
+  }
+  if (nummer9 == 'white') {
+    if (mouseX > 375 && mouseX < 500 && mouseY > 375 && mouseY < 500) {
+      nummer9 = 'blue'
+      changeturn()
+    }
+  }
+}
 }
